416066865d6833fe8b9f8db0f17c575a6ba161e6
mohamedhesham221 <mohmadetsh874@gmail.com>
2023-04-01 16:08:55 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Frontend Mentor - E-commerce product page solution
+
+This is a solution to the [E-commerce product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch the large product image by clicking on the small thumbnail images
+- Add items to the cart
+- View the cart and remove items from it
+
+### Screenshot
+
+![screenshot](./dist/images/screenshot1.png)
+![screenshot](./dist/images/screenshot2.png)
+![screenshot](./dist/images/screenshot3.png)
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+-JavaScript
+
+## Author
+
+- Frontend Mentor - [@mohamedhesham221](https://www.frontendmentor.io/profile/mohamedhesham221)
+- Linkedin - [Muhammad Hisham](https://www.linkedin.com/in/muhammad-hisham-23544b253/)
---
+++ dist/app.js
@@ -0,0 +1,178 @@
+let hamburger = document.querySelector(".hamburger"),
+  closeNav = document.querySelector(".close-nav"),
+  navbar = document.querySelector(".navbar"),
+  navItems = document.querySelector(".navbar nav"),
+  cart = document.getElementById("cart"),
+  minus = document.getElementById("minus"),
+  plus = document.getElementById("plus"),
+  shoesCounter = document.getElementById("shoesCounter"),
+  prev = document.querySelector(".prev"),
+  next = document.querySelector(".next"),
+  counter = 0,
+  warning = document.querySelector(".warning-msg"),
+  btnAdd = document.querySelector(".btn-add"),
+  numsOfItems = document.querySelector(".cart-counter span"),
+  cartItems = document.querySelector(".cart-items"),
+  arrOfItems = [],
+  thumbs = document.querySelectorAll(".thumbs img");
+
+const imgs = [
+  {
+    id: 1,
+    src: "images/image-product-1.jpg",
+  },
+  {
+    id: 2,
+    src: "images/image-product-2.jpg",
+  },
+  {
+    id: 3,
+    src: "images/image-product-3.jpg",
+  },
+  {
+    id: 4,
+    src: "images/image-product-4.jpg",
+  },
+];
+
+hamburger.onclick = function () {
+  navbar.classList.add("showNav");
+  navItems.classList.add("stretchNav");
+};
+closeNav.onclick = function () {
+  navbar.classList.remove("showNav");
+  navItems.classList.remove("stretchNav");
+};
+cart.onclick = function () {
+  document.querySelector(".cart").classList.toggle("openCart");
+};
+
+let emptyMsg = document.createElement("p");
+if (arrOfItems.length === 0) {
+  emptyMsg.textContent = "Your cart is empty";
+  emptyMsg.setAttribute("class", "empty-msg");
+  cartItems.append(emptyMsg);
+}
+let checkoutBtn = document.createElement("button");
+checkoutBtn.textContent = "Checkout";
+checkoutBtn.setAttribute("class", "checkout");
+
+plus.onclick = function () {
+  shoesCounter.value++;
+};
+minus.onclick = function () {
+  if (shoesCounter.value <= 0) {
+    shoesCounter.value = 0;
+  } else {
+    shoesCounter.value--;
+  }
+};
+next.onclick = function () {
+  counter++;
+  if (counter > 3) {
+    counter = 0;
+  }
+  document.getElementById("main-img").src = imgs[counter].src;
+};
+prev.onclick = function () {
+  counter--;
+  if (counter < 0) {
+    counter = 3;
+  }
+  document.getElementById("main-img").src = imgs[counter].src;
+};
+
+btnAdd.onclick = function () {
+  if (shoesCounter.value === "0") {
+    warning.classList.add("showWarningMsg");
+  } else {
+    warning.classList.remove("showWarningMsg");
+    document
+      .querySelector(".cart-counter span")
+      .classList.add("showBasketCount");
+    numsOfItems.innerHTML =
+      Number(numsOfItems.innerHTML) + Number(shoesCounter.value);
+
+    let cartItem = document.createElement("div");
+    cartItem.setAttribute("class", "cart-item");
+
+    let cartThumb = document.createElement("img");
+    cartThumb.setAttribute("alt", "item thumbnail");
+    let infoWraber = document.createElement("div");
+    infoWraber.setAttribute("class", "info-wrapper");
+    let productName = document.createElement("p"),
+      productPrice = document.createElement("p");
+    let delIcon = document.createElement("img");
+    delIcon.setAttribute("alt", "remove");
+    delIcon.setAttribute("class", "remove-item");
+
+    arrOfItems.push({
+      id: 0,
+      el: cartItem,
+      title: document.querySelector(".product-name").innerText,
+      thumb: document.getElementById("main-img").src,
+      price: 125.0,
+      productCount: shoesCounter.value,
+      productDel: "images/icon-delete.svg",
+    });
+
+    arrOfItems.forEach((item, index) => {
+      item.id += index;
+      cartItems.append(item.el);
+
+      cartThumb.src = item.thumb;
+      item.el.append(cartThumb);
+
+      item.el.append(infoWraber);
+      infoWraber.append(productName);
+      infoWraber.append(productPrice);
+      productName.append(item.title);
+      productName.innerHTML = productName.innerHTML.slice(0, 20) + " ...";
+      productPrice.innerHTML = `$${item.price.toFixed(2)} x ${
+        item.productCount
+      } <strong class="total">$<span>${
+        item.price * item.productCount
+      }</span>.00</strong>`;
+
+      item.el.append(delIcon);
+      delIcon.src = item.productDel;
+
+      let removeItems = document.querySelectorAll(".remove-item");
+      removeItems.forEach((removeItem) => {
+        removeItem.onclick = () => {
+          arrOfItems.pop();
+          let itemPrice =
+            removeItem.previousSibling.lastChild.lastChild.childNodes[1]
+              .innerHTML;
+          numsOfItems.innerHTML -= Number(itemPrice) / 125;
+          removeItem.parentNode.remove();
+          let itemsCount = document.querySelector(".cart-counter span");
+          if (itemsCount.innerHTML === "0") {
+            itemsCount.classList.remove("showBasketCount");
+          }
+          if (arrOfItems.length === 0) {
+            document.querySelector(".checkout").remove();
+            emptyMsg.textContent = "Your cart is empty";
+            emptyMsg.setAttribute("class", "empty-msg");
+            cartItems.append(emptyMsg);
+          }
+        };
+      });
+    });
+  }
+  if (arrOfItems.length > 0) {
+    document.querySelector(".empty-msg")?.remove();
+  }
+  if (arrOfItems.length > 0) {
+    cartItems.lastChild.after(checkoutBtn);
+  }
+};
+thumbs.forEach((thumb, index) => {
+  thumb.onclick = () => {
+    document.getElementById("main-img").src = `images/image-product-${index+1}.jpg`;
+    thumbs.forEach((thumb) => {
+    thumb.classList.remove("active");
+    })
+    thumb.classList.add("active");
+  }
+})
